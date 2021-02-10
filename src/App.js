e421eb924fb9c5bf9e20076bc0544899ab03b9c8
Victor <rusuvictor30@gmail.com>
2021-02-10 12:38:34 -0500

    Calling wp api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import React, { Component } from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import Quixote from './components/WPpdf';
 import './App.css';
-import WPpdf from './components/WPpdf';
-function App() {
-  return (
-    <WPpdf />
-  );
+
+class App extends Component {
+
+  state = {
+    post: {
+      postTitle: '',
+      postImage: '',
+      postText: '',
+      postAuthor: ''
+    },
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    fetch("http://localhost/wp-json/wp/v2/posts/1")
+      .then(res => res.json())
+        .then(
+          (wpResponse) => {
+            console.log(wpResponse.author);
+            var authorId = wpResponse.author;
+            fetch("http://localhost/wp-json/wp/v2/users/" + authorId)
+              .then(res => res.json())
+                .then(
+                  (authorResponse) => {
+                    console.log(authorResponse.name);
+                    this.setState({
+                      
+                    })
+                  }
+                )
+          },
+          (error) => {
+            this.setState({
+                isLoaded: false,
+                error
+            });
+          }
+        )
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <PDFViewer>
+          <Quixote />
+        </PDFViewer>
+      </div>
+    );
+  }
 }
 
 export default App;
